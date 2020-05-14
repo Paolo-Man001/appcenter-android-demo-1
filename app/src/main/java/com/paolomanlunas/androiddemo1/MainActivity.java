c2c0bf7a0336c2816fed3394f2b862205141fe36
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-05-14 16:37:34 +1200

    s3.20:   Create smaple-if-else-conditions to demonstrate te use of Analytics.trackEvent().   Add Analytics.trackEvents('event-name') to register(the event-name) to App Center when called/triggered by the if-conditions.

--- app/src/main/java/com/paolomanlunas/androiddemo1/MainActivity.java
+++ app/src/main/java/com/paolomanlunas/androiddemo1/MainActivity.java
@@ -8,8 +8,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+   private EditText mInterestRate;
+   private EditText mCurrentAge;
+   private EditText mRetirementAge;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +25,34 @@ public class MainActivity extends AppCompatActivity {
       AppCenter.start(getApplication(), "b848ee0d-5c05-4564-a8d2-28e7ce469f76",
               Analytics.class, Crashes.class);
 
+      mInterestRate = findViewById(R.id.interestEditText);
+      mCurrentAge = findViewById(R.id.ageEditText);
+      mRetirementAge = findViewById(R.id.retirementEditText);
 
    }
 
-//   public void calculateSavings(View view) throws Exception {
-//      throw new Exception("Something went wrong!");
-//   }
-//
    public void calculateSavings(View view){
 //      Crashes.generateTestCrash();
+      String interestRate = mInterestRate.getText().toString();
+      float interestFloat = Float.parseFloat(interestRate);
+
+      String currentAge = mCurrentAge.getText().toString();
+      int currentAgeInt = Integer.parseInt(currentAge);
+
+      String retirementAge = mRetirementAge.getText().toString();
+      int retirementAgeInt = Integer.parseInt(retirementAge);
+
+      //--- Track Event will register in App Center's Analytics.
+      // It will show the name as tag-names of the registered events.
+      if (interestFloat <= 0) {
+         // Track Event
+         Analytics.trackEvent("wrong_interest_rate");
+      }
+
+      if (retirementAgeInt <= currentAgeInt) {
+         Analytics.trackEvent("wrong_age");
+      }
+
+      Toast.makeText(this,"Interest is "+interestFloat, Toast.LENGTH_SHORT).show();
    }
 }
