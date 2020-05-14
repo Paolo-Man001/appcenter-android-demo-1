f9a79b481e9489a4b22e1864acca68411e348ae9
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-05-14 17:21:02 +1200

    s3.21: track events w/ Context :   Use 'Properties' in Analytics.trackEvent('event-name', properties).   Use Hashmap<'event-property-NAME','event-property-VALUE'> to send to AppCenter as the 'property' to see Error's-Context.

--- app/src/main/java/com/paolomanlunas/androiddemo1/MainActivity.java
+++ app/src/main/java/com/paolomanlunas/androiddemo1/MainActivity.java
@@ -7,15 +7,21 @@ import com.microsoft.appcenter.crashes.Crashes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
+   private static final String LOG_TAG = "LOG_TAGE";
    private EditText mInterestRate;
    private EditText mCurrentAge;
    private EditText mRetirementAge;
+   private EditText mMonthlySavings;
+   private EditText mCurrentSavings;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
@@ -28,31 +34,54 @@ public class MainActivity extends AppCompatActivity {
       mInterestRate = findViewById(R.id.interestEditText);
       mCurrentAge = findViewById(R.id.ageEditText);
       mRetirementAge = findViewById(R.id.retirementEditText);
+      mMonthlySavings = findViewById(R.id.monthlySavingsEditText);
+      mCurrentSavings = findViewById(R.id.currentSavingsEditText);
 
    }
 
-   public void calculateSavings(View view){
+   public void calculateSavings(View view) {
+      Log.d(LOG_TAG, "Calculate Button CLICKED!");
+      Toast.makeText(this, "Calculate Button is CLICKED! ", Toast.LENGTH_SHORT).show();
 //      Crashes.generateTestCrash();
-      String interestRate = mInterestRate.getText().toString();
-      float interestFloat = Float.parseFloat(interestRate);
+      try {
 
-      String currentAge = mCurrentAge.getText().toString();
-      int currentAgeInt = Integer.parseInt(currentAge);
+         String interestRate = mInterestRate.getText().toString();
+         float interestFloat = Float.parseFloat(interestRate);
 
-      String retirementAge = mRetirementAge.getText().toString();
-      int retirementAgeInt = Integer.parseInt(retirementAge);
+         String currentAge = mCurrentAge.getText().toString();
+         int currentAgeInt = Integer.parseInt(currentAge);
 
-      //--- Track Event will register in App Center's Analytics.
-      // It will show the name as tag-names of the registered events.
-      if (interestFloat <= 0) {
-         // Track Event
-         Analytics.trackEvent("wrong_interest_rate");
+         String retirementAge = mRetirementAge.getText().toString();
+         int retirementAgeInt = Integer.parseInt(retirementAge);
+
+         String monthlySavings = mMonthlySavings.getText().toString();
+         float monthlySavingsFloat = Float.parseFloat(monthlySavings);
+
+         String currentSavings = mCurrentSavings.getText().toString();
+         float currentSavingsFloat = Float.parseFloat(currentSavings);
+
+         //----- Use-of 'Properties' in Analytics.trackEvent()
+         // Theses 'Properties' will ALL be sent to AppCenter as 'Events' when an error is triggered.
+         // This is important to see the CONTEXT of which the error/s occurred as the user is on the app.
+         HashMap<String, String> properties = new HashMap<>();
+         properties.put("interest_rate", String.valueOf(interestFloat));
+         properties.put("current_age", currentAge);
+         properties.put("retirement_age", retirementAge);
+         properties.put("monthly_savings", String.valueOf(monthlySavingsFloat));
+         properties.put("current_savings", String.valueOf(currentSavingsFloat));
+
+         //----- Track Event will register in App Center's Analytics.
+         // It will show the name as tag-names of the registered events.
+         if (interestFloat <= 0) {
+            // Track Event
+            Analytics.trackEvent("wrong_interest_rate", properties);
+         }
+         if (retirementAgeInt <= currentAgeInt) {
+            Analytics.trackEvent("wrong_age", properties);
+         }
+      } catch (Exception e) {
+         Analytics.trackEvent(e.getMessage());
       }
 
-      if (retirementAgeInt <= currentAgeInt) {
-         Analytics.trackEvent("wrong_age");
-      }
-
-      Toast.makeText(this,"Interest is "+interestFloat, Toast.LENGTH_SHORT).show();
    }
 }
